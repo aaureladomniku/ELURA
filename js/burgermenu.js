06edd82249f2ca803f000867e7da607aa3b1b5a2
eldaahax <146485957+eldaahax@users.noreply.github.com>
2025-12-09 15:52:10 +0200

    burger menu added to pages

---
+++ js/burgermenu.js
@@ -0,0 +1,32 @@
+
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+const a=document.querySelectorAll(".l a")
+burger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+   a.forEach(a=>{
+
+      a.classList.toggle("b");
+   })
+});
+
+  let index = 0;
+  const slider = document.querySelector(".slider");
+  const posts = document.querySelectorAll(".slider .post");
+
+  const cardWidth = 300;
+  const maxIndex = posts.length - 4;
+  
+  
+
+  setInterval(() => {
+    index++;
+
+    if (index > maxIndex) {
+      index = 0;
+    }
+
+    slider.style.transform = `translateX(-${index * cardWidth}px)`;
+  }, 2500);
+
+  
