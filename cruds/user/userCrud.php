8920160f6f344916b07cf912f3a6097a30b15665
eldaahax <146485957+eldaahax@users.noreply.github.com>
2026-01-21 14:36:44 +0200

    added userCrud Class

---
+++ cruds/user/userCrud.php
@@ -0,0 +1,78 @@
+<?php
+ include_once '../SQL/DbConnection.php';
+
+class UserCrud extends DbConnection{
+
+
+
+    public function createUser($name,$email,$password){
+        $sql='INSERT INTO users (name,email,password) VALUES (?,?,?);';
+        $stmt=$this->getConn()->prepare($sql);
+             
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        
+        return $stmt->execute([$name, $email, $hashedPassword]);
+    }
+
+
+    
+    public function emailExists($email)
+{
+    $sql = 'SELECT id FROM users WHERE email = ?';
+    $stmt = $this->getConn()->prepare($sql);
+    $stmt->execute([$email]);
+
+    return $stmt->rowCount() > 0;
+}
+
+    public function getUserByEmail($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = ? ';
+        $stmt = $this->getConn()->prepare($sql);
+        $stmt->execute([$email]);
+
+        return $stmt->fetch();
+    }
+
+    
+    public function getUserById($id)
+    {
+        $sql = 'SELECT * FROM users WHERE id = ? ';
+        $stmt = $this->getConn()->prepare($sql);
+        $stmt->execute([$id]);
+
+        return $stmt->fetch();
+    }
+
+public function readUsers() {
+    $sql = "SELECT id, name, email, role, created_at FROM users";
+    $stmt = $this->getConn()->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll();
+}
+
+
+    public function deleteUser($id){
+        $sql='DELETE FROM users WHERE id=?';
+        $stmt=$this->getConn()->prepare($sql);
+       return $stmt->execute([$id]);
+    
+
+    }
+
+    
+public function updateUser($id, $name, $email, $role) {
+
+    $sql = 'UPDATE users 
+            SET name = ?, email = ?, role = ?
+            WHERE id = ?';
+
+    $stmt = $this->getConn()->prepare($sql);
+
+    return $stmt->execute([$name, $email, $role, $id]);
+}
+
+
+
+
+}
