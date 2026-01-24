fe46104bab4fc6e18451cbd90ed9d5a58682cefd
aureladomniku <aureladomniku05@gmail.com>
2026-01-24 14:19:40 +0100

    added contactClass.php

---
+++ cruds/contact/contact.php
@@ -0,0 +1,51 @@
+<?php
+
+class Contact
+{
+    private $name;
+    private $phone;
+    private $email;
+    private $message;
+
+    public function __construct($name, $phone, $email, $message)
+    {
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->email = $email;
+        $this->message = $message;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
