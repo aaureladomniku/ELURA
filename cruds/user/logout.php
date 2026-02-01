e755855a859f5f2444a5ace5f1308ffdfb095a90
eldaahax <146485957+eldaahax@users.noreply.github.com>
2026-02-01 23:58:39 +0200

    going back to dashboard last minute fix

--- cruds/user/logout.php
+++ cruds/user/logout.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 session_destroy();
+if (isset($_COOKIE['PHPSESSID'])) {
+setcookie('PHPSESSID', '', time() - 3600, '/');
+}
 header("Location: ../../html/log-in.php");
 exit();
 ?>
